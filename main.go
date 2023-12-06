c6350354868c577b42387919ab7c686eee0f0070
0x4E43 <nimaic.dev@gmail.com>
2023-12-06 20:43:26 +0530

    Basic tcp server added

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fmt.Println("Hello Joker")
+
+	//create a tcp server
+	lstn, err := net.Listen("tcp", "localhost:8080")
+
+	handleError(err)
+
+	fmt.Println("Server started:", lstn.Addr())
+	for {
+		con, err := lstn.Accept()
+		handleError(err)
+		handleConnection(con)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	fmt.Println(conn)
 }
