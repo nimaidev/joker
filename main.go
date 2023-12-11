7bbec0f212c3ba170c4568d0b349c9bc5516f171
0x4E43 <nimaic.dev@gmail.com>
2023-12-11 22:57:47 +0530

    Code Clean up and better formats

--- main.go
+++ main.go
@@ -1,53 +1,55 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
+
+	"github.com/0x4E43/joker/server"
 )
 
-func handleError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+// func handleError(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
 func main() {
 	fmt.Println("Hello Joker")
 
 	//create a tcp server
-	lstn, err := net.Listen("tcp", "localhost:8080")
+	// lstn, err := net.Listen("tcp", "localhost:8080")
 
-	handleError(err)
+	// handleError(err)
 
-	fmt.Println("Server started:", lstn.Addr())
-	for {
-		con, err := lstn.Accept()
-		handleError(err)
-		handleConnection(con)
-	}
+	// fmt.Println("Server started:", lstn.Addr())
+	// for {
+	// 	con, err := lstn.Accept()
+	// 	handleError(err)
+	// 	go ha
+
+	servOption := server.SetServerOption("8080")
+	server.CreateServer(servOption)
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	fmt.Println("Client connected: ", conn.RemoteAddr())
+// func handleConnection(conn net.Conn) {
+// 	defer conn.Close()
+// 	fmt.Println("Client connected: ", conn.RemoteAddr())
 
-	//read data from client
-	scanner := bufio.NewScanner(conn) //Initializing scanner at conn
+// 	//read data from client
+// 	scanner := bufio.NewScanner(conn) //Initializing scanner at conn
 
-	for scanner.Scan() {
-		recieved := scanner.Text()
-		fmt.Println("Recieved: ", recieved)
-		//Write into the client
-		conn.Write([]byte("\n"))
-		returnText := "Hello client from : " + conn.RemoteAddr().Network() + "\n"
-		conn.Write([]byte(returnText))
-		//TODO:  See how redis work to get commands from clients
-	}
+// 	for scanner.Scan() {
+// 		recieved := scanner.Text()
+// 		fmt.Println("Recieved: ", recieved)
+// 		//Write into the client
+// 		conn.Write([]byte("\n"))
+// 		returnText := "Hello client from : " + conn.RemoteAddr().Network() + "\n"
+// 		conn.Write([]byte(returnText))
+// 		//TODO:  See how redis work to get commands from clients
+// 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error in reading data: ", err)
-	}
+// 	if err := scanner.Err(); err != nil {
+// 		fmt.Println("Error in reading data: ", err)
+// 	}
 
-	fmt.Println("Client disconnected", conn.RemoteAddr())
-}
+// 	fmt.Println("Client disconnected", conn.RemoteAddr())
+// }
---
+++ server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+
+	"github.com/0x4E43/joker/utils"
+)
+
+type ServerOption struct {
+	port string
+}
+
+func SetServerOption(port string) *ServerOption {
+	opt := ServerOption{port}
+	return &opt
+}
+
+func CreateServer(servOption *ServerOption) {
+	//Listen to the port
+	lstnr, err := net.Listen("tcp", ":"+servOption.port)
+	utils.HandleError(err)
+	fmt.Println("Joker laighing at :", lstnr.Addr().String())
+	for {
+		conn, err := lstnr.Accept()
+		utils.HandleError(err)
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		recv := scanner.Text()
+		returnStr := "SERVER: " + recv + "\r\n"
+		fmt.Println("CLIENT: ", conn.RemoteAddr(), " : ", recv)
+		conn.Write([]byte(returnStr))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Something went wrong with Scanner")
+	}
+
+}
