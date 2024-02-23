9728f5d78256012b61f312983772e576c2c2599f
0x4E43 <nimaic.dev@gmail.com>
2024-02-23 23:29:47 +0530

    code cleanup

--- server/server.go
+++ server/server.go
@@ -1,12 +1,10 @@
 package server
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"net"
-	"strings"
 
 	"github.com/0x4E43/joker/utils"
 )
@@ -56,67 +54,8 @@ func handleConnectionV0(conn net.Conn) {
 		// log.Println(string(buf))
 		// log.Println("BUFFER: ", buf)
 		// conn.Write(parser.ProcessCLICommand(string(buf)))
-		return
-		conn.Write(buf)
+		returnStr := "OK, " + string(buf)
+		conn.Write([]byte(returnStr))
 		buf = nil
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	var commandBuffer strings.Builder
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		recv := scanner.Text()
-		// returnStr := recv + "\r\n"
-		commandBuffer.WriteString(recv + " ")
-
-		log.Println("CLIENT:", conn.RemoteAddr(), ":", recv)
-		// fmt.Println("SERVER:", returnStr)
-		if len(recv) > 1 {
-			parseCommand(recv)
-		}
-
-		if recv == "" {
-			// Process the entire command
-			command := strings.TrimSpace(commandBuffer.String())
-			if len(command) > 0 {
-				parseCommand(command)
-			}
-
-			// Clear the buffer for the next command
-			commandBuffer.Reset()
-		}
-		conn.Write([]byte(recv))
-	}
-
-	if err := scanner.Err(); err != nil {
-		if opErr, ok := err.(*net.OpError); ok {
-			if opErr.Err.Error() == "use of closed network connection" {
-				fmt.Println("Connection closed by client")
-			} else {
-				fmt.Println("Network error:", opErr.Err)
-			}
-		} else {
-			fmt.Println("Scanner error:", err)
-		}
-	}
-
-}
-
-func parseCommand(cmd string) {
-	parts := strings.Fields(cmd)
-	cmdLen := len(parts)
-	if cmdLen < 1 {
-		fmt.Println("Invalid Command")
-		return
-	}
-
-	fmt.Println(cmdLen, "arguments:")
-	for i, arg := range parts {
-		fmt.Printf("%d) \"%s\"\n", i+1, arg)
-	}
-	wholeCommand := strings.Join(parts, " ")
-	fmt.Println("Entire Command as a Single Argument:")
-	fmt.Printf("1) \"%s\"\n", wholeCommand)
 }
