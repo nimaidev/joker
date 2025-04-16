3688094374e177c2183b65cfe20a4cec9e5c2330
NimaiDev <nimaic.dev@gmail.com>
2025-04-17 00:51:44 +0530

    closing cmnd works without closing the tcp server

--- main.go
+++ main.go
@@ -23,26 +23,31 @@ func main() {
 			return
 		}
 		defer con.Close()
-		fmt.Printf("Connection details %s \n", con.RemoteAddr())
-		// need to read what client are sending
-		buffer := make([]byte, 1028)
-		for {
-			n, err := con.Read(buffer)
-			if err != nil {
-				fmt.Printf("Error %v \n", err)
-			}
-			clientBuffer := buffer[:n]
-			clientCmd := string(clientBuffer)
-			// Trim space from clientCmd
-			clientCmd = strings.TrimSpace(clientCmd) //this will remove \n or \r
-			if clientCmd == "0" {
-				con.Write([]byte("Thank you for using JOKER"))
-				con.Close()
-				return
-			}
-			fmt.Printf("Client : %s | Data : %s", con.RemoteAddr(), string(clientBuffer))
-			con.Write([]byte("OK\r\n"))
+		go handleConnection(con) //To handle connection close
 
+	}
+}
+
+func handleConnection(con net.Conn) {
+	fmt.Printf("Connection details %s \n", con.RemoteAddr())
+	// need to read what client are sending
+	buffer := make([]byte, 1028)
+	for {
+		n, err := con.Read(buffer)
+		if err != nil {
+			fmt.Printf("Error %v \n", err)
 		}
+		clientBuffer := buffer[:n]
+		clientCmd := string(clientBuffer)
+		// Trim space from clientCmd
+		clientCmd = strings.TrimSpace(clientCmd) //this will remove \n or \r
+		if clientCmd == "0" {
+			con.Write([]byte("Thank you for using JOKER"))
+			con.Close()
+			return
+		}
+		fmt.Printf("Client : %s | Data : %s", con.RemoteAddr(), string(clientBuffer))
+		con.Write([]byte("OK\r\n"))
+
 	}
 }
